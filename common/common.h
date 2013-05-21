c174dc0e3bd1d5aeb1fe4e3c5bb62d5241a94742
Jan Lepper <jan_lepper@gmx.de>
2013-05-21 13:58:54 +0200

    class DebugStream; macros debug / printvar()

---
+++ common/common.h
@@ -0,0 +1,2 @@
+#include "types.h"
+#include "debug_stream.h"
---
+++ common/debug_stream.h
@@ -0,0 +1,51 @@
+#ifndef __DEBUG_STREAM_H__
+#define __DEBUG_STREAM_H__
+
+#include "types.h"
+
+#include <iostream>
+#include <sstream>
+
+
+class DebugStream
+{
+public:
+    DebugStream() : num_items(0) {}
+    DebugStream(const char *context) : num_items(0) {
+        std::cerr << context << ": ";
+    }
+    DebugStream(int line, const char *context) : num_items(0) {
+        std::cerr << context << " - " << line << ": ";
+    }
+    ~DebugStream() {
+        std::cerr << std::endl;
+    }
+
+    template <typename t>
+    DebugStream &operator<<(t &param) {
+        if (num_items)
+            std::cerr << ' ';
+        std::cerr << param;
+        num_items++;
+        return *this;
+    }
+
+    DebugStream &operator<<(int param) {
+        if (num_items)
+            std::cerr << ' ';
+        std::cerr << param;
+        num_items++;
+        return *this;
+    }
+
+private:
+    int num_items;
+};
+
+
+// #define debug (DebugStream(__LINE__, __PRETTY_FUNCTION__ ))
+#define debug (DebugStream(__PRETTY_FUNCTION__ ))
+#define printvar(var) debug << #var << "=" << var;
+
+
+#endif // __DEBUG_STREAM_H__
