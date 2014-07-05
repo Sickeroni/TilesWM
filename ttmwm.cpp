9fec7797cfe38513877335da6e1007f1a505ef0c
Jan Lepper <jan_lepper@gmx.de>
2014-07-05 15:15:18 +0200

    compile-time flag: FRONTEND_DUMMY

--- ttmwm.cpp
+++ ttmwm.cpp
@@ -1,11 +1,37 @@
+// #define FRONTEND_DUMMY
+
 #include "backend_x11/x11_application.h"
+#ifndef FRONTEND_DUMMY
 #include "frontend/application.h"
+#endif
 
 #include "common.h"
 
+
+#ifdef FRONTEND_DUMMY
+class FrontendDummy : public FrontendBase
+{
+public:
+    virtual void init() {}
+    virtual void shutdown() {}
+    virtual void focusActiveClient() {}
+    virtual ClientFrontend *createClientFrontend(ClientBackend *backend, bool is_floating) {
+//         return new ClientFrontendDummy(backend);
+        return 0;
+    }
+    virtual void destroyClientFrontend(ClientFrontend *frontend) {}
+    virtual int numKeyGrabHandlers() { return 0; }
+    virtual KeyGrabHandlerBase *keyGrabHandler(int index) { return 0; }
+};
+#endif
+
 int main()
 {
+#ifdef FRONTEND_DUMMY
+    FrontendDummy frontend;
+#else
     Application frontend;
+#endif
 
     X11Application backend(&frontend);
 
