7f1eda2d0493c58498f799da9e330551d0b8c377
Jan Lepper <jan_lepper@gmx.de>
2014-07-08 13:09:37 +0200

    cleanup theme

--- common/theme.h
+++ common/theme.h
@@ -3,52 +3,8 @@
 
 #include "rect.h"
 
-class ContainerBase;
-class ContainerContainer;
-class ClientContainer;
-class Client;
-class Canvas;
-class Widget;
-
 namespace Theme
 {
-#if 0
-    struct ContainerContainerSizes
-    {
-        int child_frame_width;
-//         int title_height;
-        int frame_width;
-    };
-
-    struct ClientContainerSizes
-    {
-        int min_contents_width;
-        int frame_width;
-    };
-
-    const ContainerContainerSizes &containerContainerSizes();
-    const ClientContainerSizes &clientContainerSizes();
-#endif
-//     void drawWidget(Widget *widget, Canvas *canvas);
-
-#if 0
-    int calcContainerContainerTitlebarHeight(const ContainerContainer *container);
-    inline void getContainerContainerClientRect(const ContainerContainer *container,
-                                                const Rect &container_rect,
-                                                Rect &client_rect) {
-        const ContainerContainerSizes &sizes = containerContainerSizes();
-        int title_height = calcContainerContainerTitlebarHeight(container);
-
-        client_rect.set(sizes.frame_width, sizes.frame_width + title_height,
-                 container_rect.w - (sizes.frame_width * 2), container_rect.h - title_height - (sizes.frame_width * 2));
-    }
-
-    void getClientContainerClientRect(ClientContainer *container, Rect &client_rect);
-    int calcTabbarHeight(ClientContainer *container);
-    int calcVerticalTabbarHeight(ClientContainer *container);
-    int getTabAt(int x, int y, ClientContainer *container);
-#endif
-
     void calcClientFrameRect(bool has_decoration, int max_text_height, const Rect &client_rect, Rect &frame_rect);
     void calcClientClientRect(bool has_decoration, int max_text_height, const Rect &frame_rect, Rect &client_rect);
 }
