e3f908a966eca8baedf856bbfb6920e9f09d6d36
Jan Lepper <jan_lepper@gmx.de>
2014-07-08 13:03:45 +0200

    delete unused files

--- frontend/action_set.h
+++ frontend/action_set.h
@@ -7,28 +7,6 @@
 class Action;
 class ComplexAction;
 
-#if 0
-class ActionSet : public ShortcutSet::Handler
-{
-public:
-    ActionSet(std::string name);
-    virtual ~ActionSet();
-
-    virtual void initShortcuts() = 0;
-
-    const ShortcutSet *shortcuts() { return _shortcuts; }
-    void reloadShortcuts();
-
-protected:
-    void createAction(std::string action_name, int id) {
-        _shortcuts->createAction(action_name, id);
-    }
-
-private:
-    ShortcutSet *_shortcuts;
-};
-#endif
-
 class ActionSet
 {
 public:
