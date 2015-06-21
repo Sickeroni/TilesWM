d90fc9699bf806ba2b0c15cb10a6143494657851
Jan Lepper <jan_lepper@gmx.de>
2015-06-21 21:24:37 +0200

    only build floating mode

--- ttmwm.cpp
+++ ttmwm.cpp
@@ -4,10 +4,10 @@
 #ifndef FRONTEND_DUMMY
 #include "application.h"
 #include "mode.h"
-#include "mode_mixed.h"
+// #include "mode_mixed.h"
 #include "mode_simple.h"
-#include "mode_default.h"
-#include "mode_3panel.h"
+// #include "mode_default.h"
+// #include "mode_3panel.h"
 #include "themes/default/default_theme.h"
 #endif
 
@@ -38,11 +38,11 @@ int main()
 #else
     std::vector<Mode*> modes;
 
-    modes.push_back(new ModeMixed(new ModeSimple(), new Mode3Panel()));
-    modes.push_back(new ModeMixed(new ModeSimple(), new ModeDefault()));
+//     modes.push_back(new ModeMixed(new ModeSimple(), new Mode3Panel()));
+//     modes.push_back(new ModeMixed(new ModeSimple(), new ModeDefault()));
     modes.push_back(new ModeSimple());
-    modes.push_back(new ModeDefault());
-    modes.push_back(new Mode3Panel());
+//     modes.push_back(new ModeDefault());
+//     modes.push_back(new Mode3Panel());
 
     ThemeBackend *theme_backend = new DefaultTheme();
 
